cbb86a9f675bec20312dc4f011b15baddfaa00f5
DanilaEfimov <efimovdanila.de@gmail.com>
2025-03-22 21:09:46 +0400

    Add files via upload

---
+++ Overrided/console.cpp
@@ -0,0 +1,59 @@
+#include "console.h"
+#include "precompiled.h"
+
+Console::Console(QWidget *parent) : QTextEdit(parent)
+{}
+
+void Console::updateOutput(Graph *G)
+{
+    int index = MainWindow::curTabIndex();
+    QTextEdit* curEdit = MainWindow::output.at(index);
+    curEdit->setText(G->show());
+}
+
+void Console::performance()
+{
+    GraphManager::answer = "";
+    static QString argv;
+    argv = Parser::lastLine(this);
+    Graph* G = MainWindow::currentGraph();
+    int res = GraphManager::calculate(argv, G);
+
+    if(GraphManager::answer != "")
+        this->append(GraphManager::answer);
+    updateOutput(G);
+    if(res == ERROR_CODE){
+        Dialog::Error(GraphManager::answer);
+    }
+    this->terminator = 0;
+}
+
+void Console::keyPressEvent(QKeyEvent *event)
+{
+    if(!this->hasFocus() || !MainWindow::curTabIndex()){
+        return;
+    }
+
+    switch(event->key()){
+    case Qt::Key_Backspace:
+        if(this->terminator){
+            this->terminator--;
+            QTextEdit::keyPressEvent(event);
+        }
+        break;
+    case Qt::Key_Return:
+        performance();
+        QTextEdit::keyPressEvent(event);
+        break;
+    case Qt::Key_Enter:
+        performance();
+        QTextEdit::keyPressEvent(event);
+        break;
+    default:
+        if(!Parser::isSpecialChar(event->key())){
+            this->terminator++;
+            QTextEdit::keyPressEvent(event);
+        }
+        break;
+    }
+}
--- Overrided/console.h
+++ Overrided/console.h
@@ -1,1 +1,23 @@
+#pragma once
+#ifndef TEXTEDIT_H
+#define TEXTEDIT_H
 
+#include "mainwindow.h"
+
+#include <QTextEdit>
+#include <QKeyEvent>
+
+class Console : public QTextEdit
+{
+    Q_OBJECT
+    friend class MainWindow;
+public:
+    Console(QWidget* parent = nullptr);
+    static void updateOutput(Graph* G);
+protected:
+    int terminator = 0;
+    virtual void performance();
+    virtual void keyPressEvent(QKeyEvent* event) override;
+};
+
+#endif // TEXTEDIT_H
